bf6f2319631774d161fb9c430bcb2778324ff942
Stephane Boisson <sboisson@gmail.com>
2018-11-07 16:47:58 +0100

    Create base layout

--- setup.py
+++ setup.py
@@ -1,19 +1,45 @@
 import setuptools
+import sys
+import re
+import glob
+from os.path import basename, splitext
+
+__version__, = re.findall(
+    '__version__ = "(.*)"', open("src/plattus/__init__.py").read()
+)
+
+if sys.version_info[0] < 3:
+    import __builtin__ as builtins
+else:
+    import builtins
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+INSTALL_REQUIRES = ["lark-parser"]
+EXTRAS_REQUIRE = {
+    "docs": [],
+    "tests": ["coverage", "hypothesis", "pytest", "six", "pytest-benchmark"],
+}
+EXTRAS_REQUIRE["dev"] = (
+    EXTRAS_REQUIRE["tests"] + EXTRAS_REQUIRE["docs"] + ["pre-commit"]
+)
+
+
 setuptools.setup(
     name="plattus",
-    version="0.0.1",
+    version=__version__,
     author="Stéphane Boisson",
     author_email="sboisson@gmail.com",
     description="A pythonic FlatBuffers library",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    keywords=["flatbuffers"],
     url="https://github.com/sboisson/plattus",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
     classifiers=[
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
@@ -21,4 +47,11 @@ setuptools.setup(
         "Topic :: Software Development :: Pre-processors",
         "Topic :: Software Development :: Code Generators",
     ],
+    install_requires=INSTALL_REQUIRES,
+    setup_requires=["pytest-runner"],
+    tests_require=EXTRAS_REQUIRE["tests"],
+    extras_require=EXTRAS_REQUIRE,
+    zip_safe=False,
+    include_package_data=True,
+    package_data={"": ["*.lark"]},
 )
